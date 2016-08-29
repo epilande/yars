6171234bf9b58e0a3b6c2ed31b9ad308d05eb02a
Emmanuel Pilande <epilande@gmail.com>
2016-08-28 23:23:45 -1000

    Set up css variables using cssnext

---
+++ src/styles/media-query.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Media queries
+  small: 'screen and (min-width: 35.5em)', // 568px
+  medium: 'screen and (min-width: 48em)', // 768px
+  large: 'screen and (min-width: 64em)', // 1024px
+  xlarge: 'screen and (min-width: 80em)', // 1280px
+};
